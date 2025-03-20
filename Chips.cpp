6f70729bfe2694b849b8d518bb2cbe6dfbbdcd23
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-03-20 15:36:59 +0530

    Create Chips.cpp

---
+++ Chips.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int i=1;
+    while(m>=i){
+        m-=i;
+        i++;
+        if(i>n){
+            i=1;
+        }
+    }
+    cout<<m<<endl;
+}
