be97076fe922013655666e52087706d80ee991eb
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-03-29 19:37:43 +0530

    Create Sasha and Sticks.cpp

---
+++ Sasha and Sticks.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long a,b;
+    cin>>a>>b;
+    if((a/b)%2==0){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+    }
+    
+}
