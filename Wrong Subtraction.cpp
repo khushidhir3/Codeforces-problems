ee2dfab997ba69dbf53daa93772a23eac8498ca1
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-04-11 14:53:38 +0530

    Create Wrong Subtraction.cpp

---
+++ Wrong Subtraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    while(b--){
+        if(a%10!=0){
+            a--;
+        }
+        else if(a%10==0){
+            a=a/10;
+        }
+    }
+    cout<<a<<endl;
+}
