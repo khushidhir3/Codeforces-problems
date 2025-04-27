919fc1a7b27b7ba62c44f5ceae667b5865b3f0db
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-04-27 20:32:26 +0530

    Create  Little Nikita.cpp

---
+++ Little Nikita.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+         if(b<=a&&(a-b)%2==0){
+             cout<<"Yes"<<endl;
+         }
+         else{
+             cout<<"No"<<endl;
+         }
+    }
+}
