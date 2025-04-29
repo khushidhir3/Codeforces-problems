fbd2c7945fb0fc8b6c534ea382352dfaff442af9
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-04-29 15:39:36 +0530

    Create  Find Divisible.cpp

---
+++ Find Divisible.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int l,r;
+        cin>>l>>r;
+        cout<<l<<" "<<l*2<<endl;
+    }
+}
