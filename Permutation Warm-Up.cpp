f2bb43f0df0d38ae820355769ce62d6de401e193
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-05-16 20:06:34 +0530

    Create Permutation Warm-Up.cpp

---
+++ Permutation Warm-Up.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a=(n*n)/4+1;
+        cout<<a<<endl;
+    }
+}
