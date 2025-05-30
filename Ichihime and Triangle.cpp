365c433af1daefc2984f7e55a8c16374e68d75b3
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-05-30 11:10:17 +0530

    Create Ichihime and Triangle.cpp

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        int x=b,y=c,z=c;
+        cout<<x<<" "<<y<<" "<<z<<endl;
+        
+    }
+}
