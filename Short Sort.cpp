6438ff334d0995620a5264ef4c138633cad45798
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-09-04 18:39:43 +0530

    Create Short Sort.cpp

---
+++ Short Sort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s=="abc"||s=="acb"||s=="bac"||s=="cba"){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
