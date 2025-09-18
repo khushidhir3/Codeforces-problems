1b9ac574a5cd17aadddf7bd3a12b51569fe73379
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-09-18 18:55:09 +0530

    Create Square String?.cpp

---
+++ Square String?.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int n=s.size();
+        if(n%2!=0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            int mid=n/2;
+            if(s.substr(0,mid)==s.substr(mid,mid))
+            cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+            }
+        }
+    }
